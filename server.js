cbdbee04c29195e16759310ccc89f40f7c11d1e9
amuammer <amuammer@students.iugaza.edu.ps>
2020-08-09 12:28:23 +0300

    setup

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+const pg = require("pg");
+require("dotenv").config();
+
+const client = new pg.Client(process.env.DATABASE_URL);
+global.DBclient = client;
+
+// cors origin
+app.all("*", (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, PUT, PATCH, POST, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+// hello World
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "Hello World" });
+});
+
+// page not found middleware
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Sorry, page not found !" });
+})
+
+// error middleware
+app.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).send({ msg: "Sorry, something went wrong !" });
+})
+
+const PORT = process.env.PORT || 3000;
+
+client.connect()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`listening on ${PORT}`)
+    );
+  }).catch((err) => {
+    console.log(err.message);
+  });
